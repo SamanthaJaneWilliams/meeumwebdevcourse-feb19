348afce1464be74d7d6dc899b73730fef71c0376
SamanthaJaneWilliams <SamanthaJaneWilliams@users.noreply.github.com>
2019-02-10 13:05:31 +1000

    Navigation and Favicons

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,2 +1,16 @@
 cssVars();
 
+//this opens/closes the mobile nav
+
+$('.nav-promt').click(function () {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+        $('.nav-inner').attr('aria-expanded', 'true');
+
+    } else {
+
+        $('.nav-inner').attr('aria-expanded', 'false');
+    }
+})
+
